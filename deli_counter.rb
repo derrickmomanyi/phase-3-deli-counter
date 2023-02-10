9b709df27e18550227a3e076ceda99f7be63034f
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-02-10 22:56:27 +0300

    Done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+require 'pry'
+
+    def line(line)
+    array = []
+     
+    if line.length == 0
+    puts "The line is currently empty."    
+     
+     else        
+        line.each.with_index {|customer, index| array << ("#{index+1}. #{customer}") }
+         puts "The line is currently: #{array.join(" ")}"
+         
+     end
+
+    def take_a_number(array, customer)
+        array.push(customer)
+        puts ( "Welcome, #{customer}. You are number #{array.length} in line.")
+    end
+
+    def now_serving(array)
+        if array.length == 0
+            puts "There is nobody waiting to be served!"  
+        else
+            puts "Currently serving #{array[0]}."  
+            array.shift
+        end 
+    end
+
+end
+# binding.pry
+
+
+
